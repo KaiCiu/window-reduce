e022b69baefc5644d37bdadd5ab4581a205a5e33
KaiCiu Quasar <quasarkaiciu@gmail.com>
2025-03-09 13:25:04 +0000

    Edited 'main.py'

--- starter-transformation/main.py
+++ starter-transformation/main.py
@@ -1,23 +1,34 @@
 import os
 from quixstreams import Application
+from datetime import timedelta
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
+# create a Quix Streams application
+app = Application()
 
+# JSON deserializers/serializers used by default
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
 
+# consume from input topic
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+# calculate average speed using 15 second tumbling window
+sdf = sdf.apply(lambda row: row["Speed"]) \
+    .tumbling_window(timedelta(seconds=15)).mean().final() \
+        .apply(lambda value: {
+            'average-speed': value['value'],
+            'time': value['end']
+            })
 
-sdf.print()
-sdf.to_topic(output_topic)
+# print every row
+sdf = sdf.update(lambda row: print(row))
+
+# publish to output topic
+sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(sdf)
